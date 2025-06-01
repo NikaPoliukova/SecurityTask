e4563074a3cc3f1542f20461d64fa2dd6f4f80d0
NikaPoliukova <nikanarchuk@gmail.com>
2025-06-01 23:24:11 +0400

    Create REST endpoint "GET /info" that provide random stats

--- src/main/java/org/example/controller/InfoController.java
+++ src/main/java/org/example/controller/InfoController.java
@@ -1,4 +1,27 @@
 package org.example.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Random;
+
+@RestController
+@RequestMapping("api/v1")
 public class InfoController {
+    private final List<String> stats = List.of(
+            "MVC application",
+            "Spring Boot 2.7 app",
+            "Active users: 10",
+            "Version: 1.0.1",
+            "Server time: " + System.currentTimeMillis()
+    );
+
+    private final Random random = new Random();
+
+    @GetMapping("/info")
+    public String getInfo() {
+        return stats.get(random.nextInt(stats.size()));
+    }
 }
